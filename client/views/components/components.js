a64889a5346d8d29ab420bc7d041a0c1ee3ab4ff
bryan kennedy <bkennedy@smm.org>
2015-02-08 17:44:16 -0600

    Add a delete button to the components list
    This makes it lots easier to cleanup test entries on the components
    page. Using an autoform extension.

--- client/views/components/components.js
+++ client/views/components/components.js
@@ -6,7 +6,17 @@ Template.components.created = function () {
 };
 
 Template.components.helpers({
-    //
+    /**
+     * Confirm component deletion
+     */
+    beforeRemove: function () {
+        return function (collection, id) {
+            var doc = collection.findOne(id);
+            if (confirm('Really delete "' + doc.name + '"?')) {
+                this.remove();
+            }
+        };
+    }
 });
 
 Template.components.rendered = function () {
