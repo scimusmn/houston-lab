c09dfdc6ec959eb9e320c7d31709f4e48d3b9142
bryan kennedy <bkennedy@smm.org>
2015-02-11 21:51:39 -0600

    Cleaning up the footer

--- client/views/common/footer.js
+++ client/views/common/footer.js
@@ -1,19 +1,13 @@
+Template.footer.rendered = function() {
+    //
+};
+
 Template.footer.helpers({
     accountEmail: function() {
         /**
          * Return only top level pages, no sub-pages (aka pages with defined parentIds)
          */
         return Meteor.user().emails[0].address;
-        //return Pages.find({});
-        //return Pages.find({
-            //$and: [
-                //{ componentId: this._id },
-                //{ parentId: {$exists: false} }
-            //],
-        //},
-        //{
-            //sort: {order: 1}
-        //});
     }
 });
 
