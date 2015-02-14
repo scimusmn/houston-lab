420b8ade6d2e57fc2ade13032ab99b1b36ef2165
bryan kennedy <bkennedy@smm.org>
2015-02-14 17:36:47 -0600

    Slide the steps in when you click forward
    This uses a mix of the hash and sessions to increment the steps as you
    move through the content

--- client/views/components/component.js
+++ client/views/components/component.js
@@ -25,6 +25,9 @@ Template.component.helpers({
                 this.remove();
             }
         };
+    },
+    sessionUpdate: function () {
+        return Session.get('specialValue');
     }
 });
 
@@ -51,5 +54,32 @@ Template.component.events({
     'click #start-over': function(e) {
         e.preventDefault();
         Router.go('components');
+    },
+    'click #forward': function(e) {
+        console.log('this - ', this);
+        e.preventDefault();
+
+        // TODO
+        // Use the hash on both sides of this, while still setting the hash
+        // for bookmarking
+        var path = Router.current().location.get().originalUrl;
+
+        var uri = new URI(path);
+        var hash = uri.hash();
+        var stepNum;
+        if (hash) {
+            console.log('hash - ', hash);
+            stepNum = _s.toNumber(_s.strRight(hash, '-'));
+        }
+        else {
+            stepNum = 0;
+        }
+        var nextStepNum = (stepNum + 1);
+        var nextStepHash = 'step-' + _s.lpad(nextStepNum, 4, '0');
+        Session.set('specialValue', nextStepNum);
+
+        uri.hash(nextStepHash);
+        Router.go(uri.href());
+
     }
 });
