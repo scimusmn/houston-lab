f49eba5e8d369bf130a548141b897d6baa6e6611
bryan kennedy <bkennedy@smm.org>
2015-02-14 10:59:11 -0600

    Return to the component page after edit

--- client/views/components/componentEdit.js
+++ client/views/components/componentEdit.js
@@ -8,6 +8,10 @@ Template.componentEdit.events({
 
 AutoForm.addHooks(['updateComponentsForm'], {
     onSuccess: function(operation, result, template) {
-        Router.go('components');
+        // Delete 'edit' segemnt from the URL to get the parent component URL
+        var path = Router.current().location.get().path;
+        var uri = new URI(path);
+        uri.segment(-1, '');
+        Router.go(uri.href());
     }
 });
