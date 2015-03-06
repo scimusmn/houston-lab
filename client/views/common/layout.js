90dbce56ff28ab748a4f5a346aae0e68fa8edef2
bryan kennedy <bkennedy@smm.org>
2015-03-06 09:56:06 -0600

    Improve development with keyboard shortcuts
    Adding keyboard shortcuts for each button on the screen. These are only
    for development. They help organize the event code a bit better as
    well. I have to add a module to help with body level events in Meteor.
    Currently the templating engine doesn't let focusless keypresses trigger
    events within a template. Hence the body events module.

--- client/views/common/layout.js
+++ client/views/common/layout.js
@@ -5,29 +5,56 @@ Template.layout.rendered = function () {
     }
 };
 
-Template.layout.events({
-    'click .lang-switches a': function(e){
-        var selectedLanguage = e.currentTarget.id;
-        Session.set('currentLanguage', selectedLanguage);
-        if (selectedLanguage == 'en') {
-            $('.en-switch').addClass('active');
-            $('.es-switch').removeClass('active');
+/**
+ * Dev keyboard language switch
+ *
+ * Makes testing faster. Production application environment won't
+ * have a keyboard, so we don't need to worry about removing.
+ */
+Template.body.events({
+    'keyup': function(e) {
+        e.preventDefault();
+        // e
+        if (e.keyCode == '69') {
+            switchLang('en');
         }
-        if (selectedLanguage == 'es') {
-            $('.en-switch').removeClass('active');
-            $('.es-switch').addClass('active');
+        // s
+        if (e.keyCode == '83') {
+            switchLang('es');
         }
-
-        /**
-         * Stop current audio, wait for language switch in the template,
-         * and then play new language file.
-         */
-        var audio = $('audio')[0];
-        audio.pause();
-        audio.currentTime = 0;
-        setTimeout(function(){
-            audio.load();
-            audio.play();
-        }, 500);
     }
 });
+
+Template.layout.events({
+    'click .lang-switches a': function(e){
+        e.preventDefault();
+        var selectedLanguage = e.currentTarget.id;
+        switchLang(selectedLanguage);
+    }
+});
+
+function switchLang(selectedLanguage) {
+    console.log('selectedLanguage - ', selectedLanguage);
+    Session.set('currentLanguage', selectedLanguage);
+    if (selectedLanguage == 'en') {
+        $('.en-switch').addClass('active');
+        $('.es-switch').removeClass('active');
+    }
+    if (selectedLanguage == 'es') {
+        $('.en-switch').removeClass('active');
+        $('.es-switch').addClass('active');
+    }
+
+    /**
+     * Stop current audio, wait for language switch in the template,
+     * and then play new language file.
+     */
+    var audio = $('audio')[0];
+    audio.pause();
+    audio.currentTime = 0;
+    setTimeout(function(){
+            audio.load();
+            audio.play();
+            }, 500);
+}
+
--- client/views/components/component.js
+++ client/views/components/component.js
@@ -59,7 +59,6 @@ Template.component.helpers({
  * Actions to complete when the component page is first loaded
  */
 Template.component.rendered = function () {
-
     /**
      * Ignore the session if you come to the page without a hash link
      */
@@ -109,7 +108,31 @@ Template.component.rendered = function () {
  *
  * Actions to take based on user input
  */
+
+/**
+ * Dev arrow key navigation
+ *
+ * Makes testing faster. Production application environment won't
+ * have a keyboard, so we don't need to worry about removing.
+ */
+Template.body.events({
+    'keyup': function(e) {
+        event.preventDefault();
+        console.log('e - ', e.keyCode);
+        if (e.keyCode == '38') {
+            goReset();
+        }
+        if (e.keyCode == '37') {
+            goPrevious();
+        }
+        if (e.keyCode == '39') {
+            goNext();
+        }
+    }
+});
+
 Template.component.events({
+
     'mousemove': function(e) {
         Session.set('mouseCoord', 'x,y: ' + e.pageX + ', ' + e.pageY);
     },
@@ -118,6 +141,7 @@ Template.component.events({
         e.preventDefault();
         Router.go( Router.current().location.get().path + '/edit' );
     },
+
     'click .edit-step-link': function(e) {
         e.preventDefault();
         // Fix this to go to the correct edit page for this link
@@ -131,151 +155,162 @@ Template.component.events({
 
     'click #start-over': function(e) {
         e.preventDefault();
-        Session.set('currentOrder', 0);
-        Router.go('component', {
-            componentNumber: this.component.componentNumber,
-            componentLink: this.component.componentLink
-        });
-        $('div.step-container div.bounceInRight').removeClass().addClass('animated bounceOutRight');
-
-        setTimeout(function(){
-            playMedia('audio', 'bodyAudio');
-        }, 500);
+        goReset();
     },
 
     'click #back': function(e) {
         e.preventDefault();
-
-        // Stop the current step audio
-        var audio = stopMedia('audio', 'stepAudio');
-
-        var currentOrder;
-        var nextOrder;
-        if (Session.get('currentOrder')) {
-            currentOrder = Session.get('currentOrder');
-            nextOrder = (currentOrder - 1);
-        }
-        else {
-            currentOrder = 0;
-            nextOrder = 0;
-        }
-
-        console.log('BACK');
-        console.log('back: currentOrder - ', currentOrder);
-        console.log('back: nextOrder - ', nextOrder);
-
-        // Set the session for reactions in the template
-        Session.set('currentOrder', nextOrder);
-
-        var pager = $('div[data-order=' + currentOrder + ']').data('pager');
-        var prevPager = [];
-        $('div[data-pager=true]').each(function() {
-            prevPager.push($(this).data('order'));
-        });
-
-        if (pager) {
-            $('div.step-container div').hide();
-            console.log('nextOrder - ', nextOrder);
-            console.log('default - prevPager - ', prevPager);
-            prevPager = prevPager.sort(sortNumber).reverse();
-            console.log('sort - prevPager - ', prevPager);
-
-            prevPager = prevPager.filter(function (element) {
-                return element < currentOrder;
-            });
-            console.log('filtered - prevPager - ', prevPager);
-
-            $('div.step-container div').slice((prevPager[0] - 1), nextOrder).show();
-            $('div[data-order=' + nextOrder + '] div').addClass('step-active');
-        }
-        else {
-            $('div[data-order=' + currentOrder + '] div').removeClass().addClass('animated bounceOutRight');
-            $('div[data-order=' + nextOrder + '] div').removeClass().addClass('step-active');
-        }
-
-        // Set the URL with a hash to track our step progress
-        var nextOrderHash = 'step-' + _s.lpad(nextOrder, 4, '0');
-        var path = Router.current().location.get().originalUrl;
-        var uri = new URI(path);
-        uri.hash(nextOrderHash);
-        Router.go(uri.href());
-
-
-        setTimeout(function(){
-            playMedia('video', 'stepVideo');
-            playMedia(audio);
-        }, 200);
-
+        goPrevious();
     },
+
     'click #forward, click #begin': function(e) {
         e.preventDefault();
-
-        var currentOrder;
-        if (Session.get('currentOrder')) {
-            currentOrder = Session.get('currentOrder');
-        }
-        else {
-            currentOrder = 0;
-        }
-
-        // Set the session for reactions in the template
-        var nextOrder = (currentOrder + 1);
-        Session.set('currentOrder', nextOrder);
-
-        console.log('FWD');
-        console.log('fwd: currentOrder - ', currentOrder);
-        console.log('fwd: nextOrder - ', nextOrder);
-
-        // Check if the upcoming step is a pager
-        var pager = $('div[data-order=' + nextOrder + ']').data('pager');
-        if (pager) {
-            $('div.step-container div').show();
-            $('div.step-container div').slice(0, currentOrder).hide();
-        }
-
-        // Set the URL with a hash to track our step progress
-        var nextOrderHash = 'step-' + _s.lpad(nextOrder, 4, '0');
-        var path = Router.current().location.get().originalUrl;
-        var uri = new URI(path);
-        uri.hash(nextOrderHash);
-        Router.go(uri.href());
-
-        /**
-         * Load the next step
-         *
-         * Wait for a short bit so that the DOM element will be there.
-         * We need to do this for the switch from the first body section
-         * where the step divs haven't loaded yet.
-         */
-        var timeout;
-        if (currentOrder === 0) {
-            timeout = 500;
-        }
-        else {
-            timeout = 0;
-        }
-
-        setTimeout(function(){
-            //
-            // Switch video
-            //
-            playMedia('video', 'stepVideo');
-
-            // Load the audio element again.
-            // The source is changed in the template.
-            playMedia('audio', 'stepAudio');
-
-            //
-            // Animate in step text
-            //
-            $('div.step-container div').removeClass('step-active');
-            $('div[data-order=' + nextOrder + '] div').removeClass().addClass('animated bounceInRight step-active');
-
-        }, timeout);
-
+        goNext();
     }
 
 });
+
+/**
+ * Navigation
+ */
+function goReset() {
+    Session.set('currentOrder', 0);
+    Router.go('component', {
+        componentNumber: this.component.componentNumber,
+        componentLink: this.component.componentLink
+    });
+    $('div.step-container div.bounceInRight').removeClass().addClass('animated bounceOutRight');
+    setTimeout(function(){
+        playMedia('audio', 'bodyAudio');
+    }, 500);
+}
+
+function goPrevious() {
+    // Stop the current step audio
+    var audio = stopMedia('audio', 'stepAudio');
+
+    var currentOrder;
+    var nextOrder;
+    if (Session.get('currentOrder')) {
+        currentOrder = Session.get('currentOrder');
+        nextOrder = (currentOrder - 1);
+    }
+    else {
+        currentOrder = 0;
+        nextOrder = 0;
+    }
+
+    console.log('BACK');
+    console.log('back: currentOrder - ', currentOrder);
+    console.log('back: nextOrder - ', nextOrder);
+
+    // Set the session for reactions in the template
+    Session.set('currentOrder', nextOrder);
+
+    var pager = $('div[data-order=' + currentOrder + ']').data('pager');
+    var prevPager = [];
+    $('div[data-pager=true]').each(function() {
+        prevPager.push($(this).data('order'));
+    });
+
+    if (pager) {
+        $('div.step-container div').hide();
+        console.log('nextOrder - ', nextOrder);
+        console.log('default - prevPager - ', prevPager);
+        prevPager = prevPager.sort(sortNumber).reverse();
+        console.log('sort - prevPager - ', prevPager);
+
+        prevPager = prevPager.filter(function (element) {
+            return element < currentOrder;
+        });
+        console.log('filtered - prevPager - ', prevPager);
+
+        $('div.step-container div').slice((prevPager[0] - 1), nextOrder).show();
+        $('div[data-order=' + nextOrder + '] div').addClass('step-active');
+    }
+    else {
+        $('div[data-order=' + currentOrder + '] div').removeClass().addClass('animated bounceOutRight');
+        $('div[data-order=' + nextOrder + '] div').removeClass().addClass('step-active');
+    }
+
+    // Set the URL with a hash to track our step progress
+    var nextOrderHash = 'step-' + _s.lpad(nextOrder, 4, '0');
+    var path = Router.current().location.get().originalUrl;
+    var uri = new URI(path);
+    uri.hash(nextOrderHash);
+    Router.go(uri.href());
+
+
+    setTimeout(function(){
+        playMedia('video', 'stepVideo');
+        playMedia(audio);
+    }, 200);
+}
+
+function goNext() {
+    var currentOrder;
+    if (Session.get('currentOrder')) {
+        currentOrder = Session.get('currentOrder');
+    }
+    else {
+        currentOrder = 0;
+    }
+
+    // Set the session for reactions in the template
+    var nextOrder = (currentOrder + 1);
+    Session.set('currentOrder', nextOrder);
+
+    console.log('FWD');
+    console.log('fwd: currentOrder - ', currentOrder);
+    console.log('fwd: nextOrder - ', nextOrder);
+
+    // Check if the upcoming step is a pager
+    var pager = $('div[data-order=' + nextOrder + ']').data('pager');
+    if (pager) {
+        $('div.step-container div').show();
+        $('div.step-container div').slice(0, currentOrder).hide();
+    }
+
+    // Set the URL with a hash to track our step progress
+    var nextOrderHash = 'step-' + _s.lpad(nextOrder, 4, '0');
+    var path = Router.current().location.get().originalUrl;
+    var uri = new URI(path);
+    uri.hash(nextOrderHash);
+    Router.go(uri.href());
+
+    /**
+     * Load the next step
+     *
+     * Wait for a short bit so that the DOM element will be there.
+     * We need to do this for the switch from the first body section
+     * where the step divs haven't loaded yet.
+     */
+    var timeout;
+    if (currentOrder === 0) {
+        timeout = 500;
+    }
+    else {
+        timeout = 0;
+    }
+
+    setTimeout(function(){
+        //
+        // Switch video
+        //
+        playMedia('video', 'stepVideo');
+
+        // Load the audio element again.
+        // The source is changed in the template.
+        playMedia('audio', 'stepAudio');
+
+        //
+        // Animate in step text
+        //
+        $('div.step-container div').removeClass('step-active');
+        $('div[data-order=' + nextOrder + '] div').removeClass().addClass('animated bounceInRight step-active');
+
+    }, timeout);
+}
 
 /**
  * Media functions
