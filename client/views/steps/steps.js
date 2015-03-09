55775d42aedc314f8d194301c1e75555b7c86070
bryan kennedy <bkennedy@smm.org>
2015-03-09 09:41:20 -0500

    Path to the edit link for steps

--- client/views/steps/steps.js
+++ client/views/steps/steps.js
@@ -4,3 +4,15 @@ Template.steps.helpers({
         return Steps.find({}, {sort: {order: 1}});
     }
 });
+
+Template.steps.events({
+    'click .edit-step-link': function(e) {
+        e.preventDefault();
+        Router.go('stepEdit', {
+            componentNumber: this.componentNumber,
+            parentLink: this.parentLink,
+            link: this.link
+        });
+        //Router.go( Router.current().location.get().path + '/' + this.link + '/edit' );
+    },
+});
