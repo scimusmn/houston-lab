9700fe33dc1be62a66c1fb739bc9db3fc517ff33
bryan kennedy <bkennedy@smm.org>
2015-05-29 14:14:35 -0500

    Enabling the first steps of a basic screensaver

--- client/views/common/layout.js
+++ client/views/common/layout.js
@@ -3,6 +3,13 @@ Template.layout.rendered = function () {
         Session.set('currentLanguage', 'en');
         $('.en-switch').addClass('active');
     }
+    var sSaver;
+    //sSaverTimeout = 180000;
+    // Dev timeout
+    sSaverTimeout = 5000;
+
+    clearTimeout(sSaver);
+    saveScreen(sSaverTimeout);
 };
 
 /**
@@ -24,8 +31,64 @@ Template.body.events({
         }
     }
 });
+console.log('Meteor - ', Meteor);
+console.log('Meteor.settings.public - ', Meteor.settings);
 
 Template.layout.events({
+    'click #screensaver': function(e){
+        // Fade the screensaver
+        // It's important to hide the element or nothing underneath
+        // will be clickable
+        $('#screensaver').removeClass('animated fadeIn');
+        $('#screensaver').addClass('animated fadeOut');
+        setTimeout(function(){
+            $('#screensaver').hide();
+        }, 600);
+        console.log('Meteor.settings.public - ', Meteor.settings.public);
+
+        if (Session.get('componentNumber') == '0110' || Session.get('componentNumber') == '0109') {
+          Router.go('/components/0107-blood-bench');
+        }
+        else {
+          Session.set('currentOrder', 0);
+
+          // Animate out steps
+          $('div.step-container div.bounceInRight').removeClass().addClass('animated bounceOutRight');
+
+          // Make the URL match the current step
+          var nextOrder = 0;
+
+          var nextOrderHash;
+          if (nextOrder === 0) {
+            nextOrderHash = '';
+          }
+          else {
+            nextOrderHash = 'step-' + _s.lpad(nextOrder, 4, '0');
+          }
+          var path = Router.current().location.get().originalUrl;
+          var uri = new URI(path);
+          uri.hash(nextOrderHash);
+          Router.go(uri.href());
+
+          Meteor.setTimeout(function(){
+            var type = 'audio';
+            var id = 'bodyAudio';
+            var media;
+            if (typeof type=='object') {
+              media = type;
+            }
+            else {
+              media = $(type + '#' + id)[0];
+            }
+            media.load();
+            media.play();
+          }, 500);
+        }
+
+        // Restart the screensaver timer
+        clearTimeout(sSaver);
+        saveScreen(sSaverTimeout);
+    },
     'click .lang-switches a': function(e){
         e.preventDefault();
         var selectedLanguage = e.currentTarget.id;
@@ -56,5 +119,20 @@ function switchLang(selectedLanguage) {
             audio.load();
             audio.play();
             }, 500);
+}
+
+/**
+ * Display the screensaver after a timeout
+ */
+function saveScreen(sSaverTimeout) {
+  sSaver = setTimeout(function(){
+    var audio = $('audio')[0];
+    audio.pause();
+    audio.currentTime = 0;
+
+    $('#screensaver').removeClass('animated fadeOut');
+    $('#screensaver').addClass('animated fadeIn');
+    $('#screensaver').show();
+  }, sSaverTimeout);
 }
 
